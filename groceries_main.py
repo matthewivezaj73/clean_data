416da74b57263913134fdf9625004c6d077eb218
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2021-11-14 16:35:06 -0500

    Added the section that will import a datasheet.

--- groceries_main.py
+++ groceries_main.py
@@ -52,6 +52,72 @@ while not not_list:
         str(item_name) +"\',\'"+ str(sales_price) +"\',\'"+str(price_paid) +"\',\'"+ str(item_quantity)  +"\')")
     #Added a case for if the user selects i.
     elif action.lower() == "i":
+        #Truncating the data in the crm_data table.
+        my_db.executeQuery("TRUNCATE crm_data")
+        #Truncating the data in the Mailings table.
+        my_db.executeQuery("TRUNCATE Mailings")
+        #Asking the user which database they would like to work with and creating a link to the file. 
+        response_ok = False
+        #Creating an empty dictionary.
+        my_dict = {}
+        #Assinging a file path to a variable.
+        filename =  "text_files/customer_export.txt"
+        #Creating two blank lists for future use.
+        output_content = []
+        my_nondup_lines = []
+        #Opening the text file to work with.
+        with open(filename) as fold:
+            #Creating a backup copy of the CSV file before writing a new copy of it.
+            for temp_line in fold:
+                #Replacing the # in the temp_line with noting.
+                temp_line = temp_line.replace('#', "")
+                #Making a split at the | character.
+                temp_line = temp_line.split("|")
+                #Checking to see if the length of the temp_line is equal to 10.
+                if len(temp_line) == 10:
+                    #If the value at index 0 is first_name, then all we are doing is assigning it a value.
+                    if temp_line[0] == "first_name":
+                        #Creating a formatted line
+                        line = f"{temp_line[0]}, {temp_line[1]}, {temp_line[2]}, {temp_line[3]}, {temp_line[4]}, {temp_line[5]}, {temp_line[6]}, {temp_line[7]}, {temp_line[8]}, {temp_line[9]}\n"
+                    #Else, we will append all other values to a list.
+                    else:
+                        #Creating a formatted line
+                        line = f"\"{temp_line[0]}\", \"{temp_line[1]}\", \"{temp_line[2]}\", \"{temp_line[3]}\", \"{temp_line[4]}\", \"{temp_line[5]}\", \"{temp_line[6]}\", \"{temp_line[7]}\", \"{temp_line[8]}\", \"{temp_line[9]}\"\n"
+                        #Appending each line to a list.
+                        output_content.append(line)
+                #Else, if the length of the temp_line is not equal to 10.
+                else:
+                    #Checking to see if the first line is first_name.
+                    if temp_line[0] == "first_name":
+                        #Creating a formatted line.
+                        line = f"{temp_line[0]}, {temp_line[1]}, {temp_line[2]}, {temp_line[3]}, {temp_line[4]}, {temp_line[5]}, {temp_line[6]}, {temp_line[7]}, {temp_line[8]}, {temp_line[9]},  {temp_line[10]}\n"
+                    else:
+                        #Creating a formatted line.
+                        line = f"\"{temp_line[0]}\", \"{temp_line[1]}\", \"{temp_line[2]}\", \"{temp_line[3]}\", \"{temp_line[4]}\", \"{temp_line[5]}\", \"{temp_line[6]}\", \"{temp_line[7]}\", \"{temp_line[8]}\", \"{temp_line[9]}\",  \"{temp_line[10]}\"\n"
+                ####BEGIN REMOVAL OF DUPLICATE LINES####
+                #Setting a flag
+                matched_word = False
+                #Looping through every entry in my_nondup_lines
+                for my_line in my_nondup_lines:
+                    #Checking for the address
+                    if temp_line[3] in my_line:
+                        #Printing a message so that I am telling the user that I am deleting the customer with a specified address value.
+                        print(f"removing a duplicate line with address: {temp_line[3]}")
+                        #Setting our flag to true.
+                        matched_word = True
+                        #Breaking out of the loop
+                        break
+                #Checking to see if the matched word is true.
+                if not matched_word:
+                    #Appending each line to the list.
+                    my_nondup_lines.append(line)
+            #Opening the file for writing/creating if it does not exist.
+            with open("text_files/customers.csv","w+") as fawn:
+                #Going through each line in the list.
+                for line in my_nondup_lines:
+                    #Writing each line to the file.
+                    fawn.write(line)             
+
     #Added a case for if the user selects c.
     elif action.lower() == "c":
     #Added a case for if the user selects s.
